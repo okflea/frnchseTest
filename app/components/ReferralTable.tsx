91995188e41fbd65503c202f56bdab810ef092fd
okflea <rohit@plz.no>
2023-07-20 11:37:59 +0530

    2

--- app/components/ReferralTable.tsx
+++ app/components/ReferralTable.tsx
@@ -1,7 +1,34 @@
+import { columns } from "../master/referral/columns"
+import { DataTable } from "../master/referral/data-table"
 
-const ReferralTable = () => {
+type ReferralTableProps = {
+  token: string
+}
+const ReferralTable : React.FC<ReferralTableProps> = async ({token}) => {
+  const getData = async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_URL
+    const response = await fetch(`${baseUrl}/api/franchisee/referral`, {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+        "Accept": "application/json",
+      },
+    })
+    if (!response.ok)
+      throw new Error("error fetching data")
+    const referralData = await response.json()
+    return referralData
+  }
+  const refData = await getData()
+
   return (
-    <div>ReferralTable</div>
+    <>
+      <div>
+        <DataTable columns={columns} data={refData} />
+      </div>
+    </>
   )
 }
 
--- app/master/referral/page.tsx
+++ app/master/referral/page.tsx
@@ -1,34 +1,13 @@
-import { columns } from "./columns"
-import { DataTable } from "./data-table"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+"use client"
+import ReferralTable from "@/app/components/ReferralTable"
+import { useSession } from "next-auth/react"
 
 const page = async () => {
 
-  const session = await getServerSession(authOptions)
-  const getData = async () => {
-    const baseUrl = process.env.NEXT_PUBLIC_URL
-    const response = await fetch(`${baseUrl}/api/franchisee/referral`, {
-      cache: "no-store",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${session?.user?.accessToken}`,
-        "Accept": "application/json",
-      },
-    })
-    if (!response.ok)
-      throw new Error("error fetching data")
-    const referralData = await response.json()
-    return referralData
-  }
-  const refData = await getData()
-
+  const { data: session } = useSession()
   return (
     <>
-      <div>
-        <DataTable columns={columns} data={refData} />
-      </div>
+      {session && <ReferralTable token={session?.user.accessToken} />}
     </>
   )
 }
